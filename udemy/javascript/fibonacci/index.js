d192b58e4d5308343f4a6ff9c195c6a5ab5f5c88
Jonrw97 <jonrw97@gmail.com>
2022-10-27 10:51:19 +0200

    Dom basics

--- udemy/javascript/fibonacci/index.js
+++ udemy/javascript/fibonacci/index.js
@@ -6,7 +6,7 @@ function fibonacciGenerator(n) {
     return [0, 1];
   } else {
     var numbers = [0, 1];
-    for (var i = 3; i !== n + 1; i++) {
+    for (var i = 3; i <= n + 1; i++) {
       var newNumber = numbers[numbers.length - 1] + numbers[numbers.length - 2];
       numbers.push(newNumber);
     }
