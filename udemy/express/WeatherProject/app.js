fbf8bcac294ff7f5513fe501ac852595f1b03bad
Jonrw97 <jonrw97@gmail.com>
2022-12-13 18:52:28 +0200

    Weather app bootstrap

--- udemy/express/WeatherProject/app.js
+++ udemy/express/WeatherProject/app.js
@@ -4,6 +4,9 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", function (req, res) {
@@ -15,12 +18,7 @@ app.post("/", function (req, res) {
   const apiKey = "55bc864beb4650efa493633a0db852bc";
   const unit = "metric";
   const url =
-    "https://api.openweathermap.org/data/2.5/weather?q=" +
-    query +
-    "&appid=" +
-    apiKey +
-    "&units=" +
-    unit;
+    "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
 
   https.get(url, function (response) {
     response.on("data", function (data) {
@@ -28,16 +26,7 @@ app.post("/", function (req, res) {
       const temp = weatherData.main.temp;
       const weather = weatherData.weather[0].description;
       const icon = weatherData.weather[0].icon;
-      res.write(
-        "<h1>The weather in " + query + " is " + temp + " degrees Celcius</h1>"
-      );
-      res.write(
-        "<h2>The weather description in " + query + " is " + weather + ".</h2>"
-      );
-      res.write(
-        "<img src = http://openweathermap.org/img/wn/" + icon + "@2x.png>"
-      );
-      res.send();
+      res.render(__dirname + "/weather.html", { query: query, temp: temp, weather: weather, icon: icon });
     });
   });
 });
